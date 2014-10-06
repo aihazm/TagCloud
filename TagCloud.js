1d007f2e3c32bc23eb097e8883f46c0b1582c8a4
Aiham Azmeh <aihazm@yahoo.com>
2014-10-06 20:08:23 +0200

    Initial commit

---
+++ TagCloud.js
@@ -0,0 +1,47 @@
+define(["jquery", "text!./tagcloud.ng.html"], function($, template) {'use strict';
+
+	return {
+		template : template,
+		initialProperties : {
+			version : 1.0,
+			qHyperCubeDef : {
+				qDimensions : [],
+				qMeasures : [],
+				qInitialDataFetch : [{
+					qWidth : 2,
+					qHeight : 50
+				}]
+			}
+		},
+		definition : {
+			type : "items",
+			component : "accordion",
+			items : {
+				dimensions : {
+					uses : "dimensions",
+					min : 1,
+					max : 1
+				},
+				measures : {
+					uses : "measures",
+					min : 1,
+					max : 1
+				},
+				sorting : {
+					uses : "sorting"
+				},
+				settings : {
+					uses : "settings"
+				}
+			}
+		},
+		snapshot : {
+			canTakeSnapshot : true
+		},
+		controller : ['$scope', function($scope) {
+			
+		}]
+
+	};
+
+});
